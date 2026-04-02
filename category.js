40620658c5ca35b1306d3ce3cc29af524785eaf4
amine sakhri <sakhriamine2003@gmail.com>
2026-04-03 00:25:36 +0200

    fait function category pour filtrer les cards

--- category.js
+++ category.js
@@ -1,41 +1,48 @@
+const category = document.getElementById("category");
 
-const category =document.getElementById("category")
+export function Affichercategory() {
+  category.innerHTML = `
+    <div class="flex gap-4 flex-wrap justify-center mt-6">
 
-export function Affichercategory(){
+      <button data-cat="Action" class="px-6 py-2 bg-[#2a2a2a] text-white rounded-full hover:bg-orange-400 transition">
+        Action
+      </button>
 
-category.innerHTML = `
+      <button data-cat="RPG" class="px-6 py-2 bg-[#2a2a2a] text-white rounded-full hover:bg-orange-500 transition">
+        RPG
+      </button>
 
-<div class="flex gap-4 flex-wrap justify-center mt-6">
+      <button data-cat="FPS" class="px-6 py-2 bg-[#2a2a2a] text-white rounded-full hover:bg-orange-500 transition">
+        FPS
+      </button>
 
-  <button id="Action" class="px-6 py-2 bg-[#2a2a2a] text-white  rounded-full hover:bg-orange-400 transition">
-    Action
-  </button>
+      <button data-cat="all" class="px-6 py-2 bg-gray-500 text-white rounded-full">
+        Tous
+      </button>
 
-  <button  id="RPG" class="px-6 py-2 bg-[#2a2a2a] text-white rounded-full hover:bg-orange-500  transition">
-    RPG
-  </button>
-
-  <button  id="FPS" class="px-6 py-2 bg-[#2a2a2a] text-white  rounded-full hover:bg-orange-500  transition">
-    FPS
-  </button>
-
-</div>
-
-
-`
-// }
-
-// const Action = document.getElementById("Action");
-// const RPG = document.getElementById("RPG");
-// const FPS = document.getElementById("FPS");
+    </div>
+  `;
+}
 
 
-// export function categoryAction(){
-// Action.onclick = () => {
-// const resultAction = games.filter(game =>
-// game.category.toLowerCase().includes(value)
-//   ); 
-// };}
+export function FiltrerLaCatigory(games, afficherGames) {
+  
+  category.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
 
+      const categorie = e.target.dataset.cat;
+
+      if (categorie === "all") {
+        afficherGames(games);
+      } else {
+        const result = games.filter(game =>
+          game.category.toLowerCase() === categorie.toLowerCase()
+        );
+
+        afficherGames(result);
+      }
+
+    }
+  });
 
 }
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { games } from "./data.js";
 import { afficherGames } from "./UI.js";
 import { SearchInput } from "./search.js";
-import { Affichercategory } from "./category.js";
-
-
+import { Affichercategory, FiltrerLaCatigory } from "./category.js";
 
 afficherGames(games);
 SearchInput();
 Affichercategory();
+
+FiltrerLaCatigory(games, afficherGames);
